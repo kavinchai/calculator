223ada47a3dd753ec72e7aa9674e76f94fc72574
Kavin Chaisawangwong <kavinchai00@gmail.com>
2022-07-08 23:30:52 -0500

    Implemented css for calc layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const calcBtnVal = [
+    ["C", "+/-", "%", "/"],
+    [7, 8, 9, "X"],
+    [4, 5, 6, "-"],
+    [1, 2, 3, "+"],
+    [0, ".", "="],
+  ];
+  return (
+    <div className="app">
+      <div className="calcContainer">
+        <div className="calcDisplay"></div>
+        <div className="calcBtnContainer">
+          {calcBtnVal.map((row, rowIndex) => (
+            <div className={`row row${rowIndex}`}>
+              {row.map((btn, btnIndex) => (
+                <button
+                  className={`btn btn${btnIndex}`}
+                  onClick={() => {
+                    console.log(btn);
+                  }}
+                >
+                  {btn}
+                </button>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
