199103d5770cc9b09b3225ccec615514c10142d7
Kavin Chaisawangwong <kavinchai00@gmail.com>
2022-07-13 10:58:01 -0500

    Implemented handleInvert/handleReset/handleNum functions

--- src/App.js
+++ src/App.js
@@ -3,21 +3,72 @@ import "./App.css";
 import { calcBtnVal } from "./constants";
 
 const App = () => {
+  const [displayNum, setDisplayNum] = useState("");
   const [inputList, setInputList] = useState([]);
-  const handleBtnClick = (btn) => {
+  const handleReset = (btn) => {
+    console.log(btn);
+    setInputList([]);
+  };
+  const handleInvert = (btn) => {
+    console.log(btn);
+    if (inputList.length > 0) {
+      if (inputList[0] !== "-") {
+        setInputList(["-", ...inputList]);
+      } else {
+        setInputList(inputList.slice(1));
+      }
+    }
+  };
+  const handlePercent = (btn) => {
+    console.log(btn);
+  };
+  const handleEquals = (btn) => {
+    console.log(btn);
+  };
+  const handleSign = (btn) => {
+    console.log(btn);
+  };
+  const handleDecimal = (btn) => {
     console.log(btn);
     setInputList((inputList) => [...inputList, btn]);
   };
+  const handleNum = (btn) => {
+    console.log(btn);
+    setInputList((inputList) => [...inputList, btn]);
+  };
+
+  useEffect(() => {
+    let result = "";
+    for (let i = 0; i < inputList.length; i++) {
+      result = result.concat(inputList[i]);
+    }
+    setDisplayNum(result);
+  }, [inputList]);
+
   return (
     <div className="app">
       <div className="calcContainer">
-        <div className="calcDisplay"></div>
+        <div className="calcDisplayComponent">
+          <span className="calcDisplay">{displayNum}</span>
+        </div>
         <div className="calcBtnContainer">
           {calcBtnVal.map((btn, btnIndex) => (
             <button
               className={`btn btn${btnIndex}`}
               onClick={() => {
-                handleBtnClick(btn);
+                btn === "C"
+                  ? handleReset(btn)
+                  : btn === "+/-"
+                  ? handleInvert(btn)
+                  : btn === "%"
+                  ? handlePercent(btn)
+                  : btn === "="
+                  ? handleEquals(btn)
+                  : btn === "/" || btn === "X" || btn === "-" || btn === "+"
+                  ? handleSign(btn)
+                  : btn === "."
+                  ? handleDecimal(btn)
+                  : handleNum(btn);
               }}
             >
               {btn}
